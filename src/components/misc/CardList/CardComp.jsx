754183fdc06588ebb8648abd90502740b6c12ce2
Anthony.M <marcellin.anthony77@gmail.com>
2022-12-24 16:36:29 +0100

    Réparation du Style des cartes de l'accueil

--- src/components/misc/CardList/CardComp.jsx
+++ src/components/misc/CardList/CardComp.jsx
@@ -1,17 +1,15 @@
 import './Card.css';
 
 import React from 'react';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function CardComp({data}) {
     return (
-        <Link to={`/appartment/${data.id}`} className='Card-Link'>
-            <div className='Card-Content'>
-                <img className='Card-Image' src={data.cover} alt={data.title} />
-                <h3 className='Card-Title'>
-                    {data.title}
-                </h3>
-            </div>
+        <Link to={`/appartment/${data.id}`} className='Card-Link Card-Shape'>
+            <img className='Card-Image' src={data.cover} alt={data.title} />
+            <h3 className='Card-Title'>
+                {data.title}
+            </h3>
         </Link>
     );
 }
