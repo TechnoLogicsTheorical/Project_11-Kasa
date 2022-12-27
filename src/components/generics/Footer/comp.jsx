e4744ffa3caeaa0e292cc40a2644c26c01765852
Anthony.M <marcellin.anthony77@gmail.com>
2022-12-27 09:39:03 +0100

    Documentation des composants Header et Footer

--- src/components/generics/Footer/comp.jsx
+++ src/components/generics/Footer/comp.jsx
@@ -2,8 +2,12 @@ import './comp.css';
 import LogoWhite from '../../../assets/LogoWhite.png';
 import React from 'react';
 
+/**
+ * Composant destinée à l'affichage du pied de page
+ * @returns {JSX.Element} Un élément HTML contenant une image et un texte de copyright
+ * @constructor
+ */
 function Footer() {
-
     return (
         <footer className='Footer'>
             <img className='Footer-Logo' src={LogoWhite} alt="Logo de la marque Kasa (Blanc)" />
--- src/components/generics/Header/comp.jsx
+++ src/components/generics/Header/comp.jsx
@@ -1,13 +1,19 @@
-import logo from '../../../assets/logo.png';
+import Logo from '../../../assets/logo.png';
 import './comp.css';
-import React from 'react';
-import {NavLink} from "react-router-dom";
 
+import React from 'react';
+import { NavLink } from "react-router-dom";
+
+/**
+ * Composant destiné à l'affichage du haut de page avec une gestion de navigation
+ * @returns {JSX.Element} Affiche le logo, avec une barre de navigation contenant des liens
+ * @constructor
+ */
 function Header() {
     return (
         <header className='Header'>
             <NavLink to="/">
-                <img className='Header-Logo' src={logo} alt="Logo Kasa" />
+                <img className='Header-Logo' src={Logo} alt="Logo Kasa" />
             </NavLink>
             <nav>
                 <NavLink to="/" className='Header-Link'>Accueil</NavLink>
